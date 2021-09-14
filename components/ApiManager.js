4ec7890cb0fc2c396b15368961859007d50c1aa5
ts <shanksleslietrey@gmail.com>
2021-09-14 14:38:34 -0500

    added state for builder form

--- components/ApiManager.js
+++ components/ApiManager.js
@@ -18,3 +18,16 @@ export const getFetch = (url) => {
     return fetch(url)
     .then(res => res.json())
 }
+
+export const putFetch = (url, obj) => {
+    return fetch(url, {
+        method: 'PUT',
+        body: JSON.stringify({
+            obj
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(res => res.json())
+    .then(json => console.log(json))
+}
---
+++ components/baboonBuilder/BaboonBuilderForm.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import { postFetch, getFetch, putFetch } from "../ApiManager"
+import "./BaboonBuilderForm.css"
+
+
+export const BaboonBuilderForm = () => {
+ const [routine1, setRoutine1] = useState("")
+ const [routine2, setRoutine2] = useState("")
+ const [routine3, setRoutine3] = useState("")
+    
+    // useEffect(
+    //     () => {
+    //          getFetch(`http://localhost:8088/baboonRoutines`)
+    //         .then((data) => {
+    //             getAddedRoutines(data)
+    //             console.log("routines:", data)
+    //         })
+    //     },
+    //     []
+    // )
+    // const addRoutine = (event) => {
+    //     event.preventDefault()
+    //     const obj = {
+    //         userId: parseInt(localStorage.getItem("baboon_user")),
+    //         firstRoutine: routines.firstRoutine
+    //     }
+
+    //     postFetch("http://localhost:8088/baboonRoutines", obj)
+    //         .then(() => {
+             
+    //         })
+    
+    // const editRoutine = (event) => {
+    //     event.preventDefault()
+    //     const obj = {
+    //         userId: parseInt(localStorage.getItem("baboon_user")),
+    //         firstRoutine: routines.firstRoutine,
+    //         secondRoutine: "",
+    //         thirdRoutine: "",
+    //         fourthRoutine: "",
+    //         fifthRoutine: ""
+    //     }
+
+    //     postFetch("http://localhost:8088/baboonRoutines", obj)
+    //         .then(() => {
+             
+    //         })
+    // }
+    // const deleteRoutine = (id) => {
+    //     fetch(`http://localhost:8088/employees/${id}`, {
+    //         method: "DELETE"
+    //     })
+    //     .then(getEmployees)
+    // }
+    return (
+       <>    
+        <form className="ticketForm">
+            <h2 className="ticketForm__title">Add Routines To Baboon Routine</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="description">Routine (word routine as a true/false e.g. "meditate 30 minutes a day"):</label>
+                    <div>
+                    <input
+                        value={routine1}
+                        onChange={
+                            (event) => {
+                                setRoutine1(event.target.value)
+                            }
+                        }
+                        type="text"
+                        placeholder="enter routine 1 here"
+                         />
+                    </div>
+                    <div>
+                    <input
+                        value={routine2}
+                        onChange={
+                            (event) => {
+                                setRoutine2(event.target.value)
+                            }
+                        }
+                        type="text"
+                        placeholder="enter routine 1 here"
+                         />
+                    </div>
+                    <div>
+                    <input
+                        value={routine3}
+                        onChange={
+                            (event) => {
+                                setRoutine3(event.target.value)
+                            }
+                        }
+                        type="text"
+                        placeholder="enter routine 1 here"
+                         />
+                    </div>
+                </div>
+            </fieldset>
+        </form>
+                {
+                    
+                            <button 
+                            >Submit This Routine</button>
+                            
+                }
+                <div className="form_state">
+                    <div>{routine1}</div>
+                    <div>{routine2}</div>
+                    <div>{routine3}</div>
+                </div>
+                
+                </>   
+        )
+
+}
